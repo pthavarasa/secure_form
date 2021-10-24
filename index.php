5bfcb829803afe419100169a2bc252856fcd2610
Thavarasa Prasanth <45243326+pthavarasa@users.noreply.github.com>
2021-10-24 16:56:30 +0200

    buttons work!

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php 
+  if(isset($_POST["create"])){
+    echo "redirect to create account";
+  }else if(isset($_POST["sign_in"]) && isset($_POST["email"]) && isset($_POST["password"])){
+    echo "handle validation";
+  }else{
+    echo "handle invalid input!";
+  }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -15,23 +25,25 @@
   </head>
   <body>
     <main class="form-signin">
-      <form>
+      <form action="index.php" method="POST">
         <img class="mb-4" src="shield.png" alt="shield" width="100">
 
         <div class="form-floating">
-          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+          <input type="email" name="email" class="form-control" id="floatingInput" placeholder="name@example.com">
           <label for="floatingInput">Email address</label>
         </div>
         <div class="form-floating">
-          <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+          <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password">
           <label for="floatingPassword">Password</label>
         </div>
 
-        <button class="w-30 btn btn-lg btn-primary" type="submit">Reset</button>
-        <button class="w-40 btn btn-lg btn-primary" type="submit">Sign in</button>
-        <button class="w-30 btn btn-lg btn-primary" type="submit">Create</button>
+        <button class="w-30 btn btn-lg btn-primary" type="reset">Reset</button>
+        <button class="w-40 btn btn-lg btn-primary" name="sign_in" type="submit">Sign in</button>
+        <button class="w-30 btn btn-lg btn-primary" name="create" type="submit">Create</button>
       </form>
     </main>
+
+    <!-- custom script -->
     <script type="text/javascript" src="custom.js"></script>
   </body>
 </html>
